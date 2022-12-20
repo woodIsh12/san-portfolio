47718fe46373e3b47fa59722efc1a049a2baa39f
londono19 <slondono.2007@hotmail.com>
2022-12-20 01:01:38 -0800

    prototype navBar

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -2,14 +2,14 @@ import React from "react";
 
 const Intro = (props)=>{
     return (
-        <div className="flex items-center justify-center
-        flex-col text-center pt-20 pb-6">
-            <h1 className="text-4xl md:text-7x1 
-            mb-1 md:3 font-bold">Santiago</h1>
-            <p className="text-base md:text-xl 
-            mb-3 font-medium">Software Developer</p>
-            <p className="text-sm max-w-xl font-bold">This is my entire bio</p>
-        </div>
+        <nav className="navBar">
+            <ul className="navUnordered">
+                <li className="navHome">Home</li>
+                <li className="">About</li>
+                <li className="">Pojects</li>
+                <li className="">Contact</li>
+            </ul>
+        </nav>
     )
 };
 
