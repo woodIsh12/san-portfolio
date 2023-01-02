dd07b01aca6097aca584a5e2fc658455c574e312
londono19 <slondono.2007@hotmail.com>
2023-01-01 23:50:23 -0800

    added percentage bars, and inline styling

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -5,6 +5,17 @@ const Skills = (props)=>{
         <>
         <section className="skills">
             <div className="skillBar">
+                <p>React.JS</p>
+                <div style={{height:"24px",width:"25%"}}></div>
+                <p>MongoDB</p>
+                <div style={{height:"24px",width:"25%"}}></div>
+                <p>Express.JS</p>
+                <div style={{height:"24px",width:"25%"}}></div>
+                <p>JAVA</p>
+                <div style={{height:"24px",width:"25%"}}></div>
+                <p>C++</p>
+                <div style={{height:"24px",width:"25%"}}></div>
+
             </div>
         </section>
         </>
