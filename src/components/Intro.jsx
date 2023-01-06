45e25e3f19382151f3b0fe303b2362ace2b3825c
londono19 <slondono.2007@hotmail.com>
2023-01-05 19:16:17 -0800

    added some projects, plus fixed more styles on the navbar

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -8,9 +8,10 @@ const Intro = (props)=>{
             <div className="container-intro">
                 <div className="my-info">
                     <h1>Santiago Londono</h1>
-                    <p>I'm an aspiring Software Developer
-                        who specializes on writing front-end. 
-                        &#40;Though I've written full-stack apps&#41;
+                    <p>I'm a Software Developer
+                        who specializes on writing full-stack applications.
+                        Currently I'm focused on perfecting my express.js and React.js 
+                        knowledge, and would like to learn Three.js {"( WebGL seems like so much fun.)"}
                     </p>
                     <button>Contact Me</button>
                 </div>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,8 +2,32 @@ import React from "react";
 
 const Projects = (props)=>{
     return (
-        <section className="projects-container">
+        <>
+        <div className="skills">
             <h2>Projects</h2>
+        </div>
+        <section className="projects-container">
+            <div className="row">
+            <div className="project-col movier">
+                <div className="layer">
+                    <h3>Movier</h3>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat saepe laudantium aperiam neque dolores alias provident quisquam natus, optio aliquam!</p>
+                </div>
+            </div>
+            <div className="project-col shareThou">
+                <div className="layer">
+                    <h3>PhilThoughts</h3>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat saepe laudantium aperiam neque dolores alias provident quisquam natus, optio aliquam!</p>
+                </div>
+            </div>
+            <div className="project-col">
+                <div className="layer">
+                    <h3>My portfolio</h3>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat saepe laudantium aperiam neque dolores alias provident quisquam natus, optio aliquam!</p>
+                    </div>
+            </div>
+
+            </div>
             <div className="row">
             <div className="project-col movier">
                 <div className="layer">
@@ -26,6 +50,7 @@ const Projects = (props)=>{
 
             </div>
         </section>
+        </>
     )
 };
 
