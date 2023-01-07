d52c5049ddcbde4a9c738b5cd7def4a718290a64
londono19 <slondono.2007@hotmail.com>
2023-01-06 21:32:37 -0800

    fixed the styles for the projects section

--- src/components/ProjectsCard.jsx
+++ src/components/ProjectsCard.jsx
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
+
+
+
 
 const ProjectsCard = (props)=>{
+
+    const [onProject, setOnProject] = useState(true);
+
+    const styleOnClick = {
+        backgroundColor: "#0e0074b7",
+    }
+
+    const changeStyle = ()=>{
+        setOnProject(!onProject);
+    }
     return(
         <div className="project-col" style={{backgroundImage: `url(${props.info.image})`}}>
-        <div className="layer">
+        <div onClick={changeStyle} className={onProject ? "layer" : "layerPurple"} >
             <h3>Movier</h3>
             <p>Lorem ipsum, dolor sit amet consectetur
                 adipisicing elit. Quaerat saepe laudantium
