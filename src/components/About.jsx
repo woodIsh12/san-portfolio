8e4b906c1cbcb0d8d44b7fc7b6aaca81641c9542
londono19 <slondono.2007@hotmail.com>
2023-01-14 23:31:51 -0800

    removed the resume, and added an option to download, also added an about me section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -16,11 +16,25 @@ const About = (props)=>{
         <>
       <div data-aos-duration="600" data-aos="fade-up" ref={props.resume} className="about">
         <div className="skills">
-            <h2>Resume</h2>
+            <h2>About Me</h2>
         </div>
         <div className="content">
-
-        <embed alt="Santiago Resume" src={resume} className="pdfViewer" type="application/pdf" />. 
+          <p style={{color: "white"}}>I'm Santiago, a Langara College student.
+          <br/>
+          During my college years I gained the skills needed to help design, develop, test and maintain
+          Software. Ranging from beautiful front-end websites, to a ful-stack application, working with 
+          several moving parts {"( microservices, API's)"}.
+          <br/>
+          I love the puzzle that software development offers. Not only this,
+          but I'm also excited expand my knowledge by working with like-minded people
+          with more experience.
+          <br/>
+          On a personal level I'd say I'm a someone who is very interested in the world.
+          what it has to offer, and how other people perceive it. I enjoy
+            philosophy, learning languages, and being outdoors. </p>
+        <a href={resume} download>
+          <button>Download resume</button>
+        </a>        
         </div>
         <Footer/>
         </div>
